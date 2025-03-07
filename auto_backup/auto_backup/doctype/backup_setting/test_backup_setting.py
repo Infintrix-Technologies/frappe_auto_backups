34ab7083b4aa33731604a3160858d0a65578dac6
MSulemanSaeed <salmansaeed7272@gmail.com>
2025-03-07 13:20:44 +0500

    update

---
+++ auto_backup/auto_backup/doctype/backup_setting/test_backup_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Suleman Saeed and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBackupSetting(FrappeTestCase):
+	pass
