0c1e126e4b1b65654a2b5f4831852b3491a6b831
MSulemanSaeed <salmansaeed7272@gmail.com>
2025-03-10 14:40:02 +0500

    update the backup scritp

--- auto_backup/hooks.py
+++ auto_backup/hooks.py
@@ -18,7 +18,7 @@ app_license = "mit"
 
 scheduler_events = {
     "cron": {
-        "* * * * *": [
+        "*/15 * * * *": [
             "auto_backup.api.backup_script.take_backup"
         ]
     }
